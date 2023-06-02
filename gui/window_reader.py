385fd00f712dc80277e4d1d825ae1d8b2d23c51c
Etwhal <xanafication@gmail.com>
2023-06-02 11:41:14 +0200

    added a button to take a screenshot

--- gui/window_reader.py
+++ gui/window_reader.py
@@ -1,17 +1,35 @@
 from tkinter import *
+from pathlib import Path
+
+import pyautogui
+import os
 
 window = Tk()
+path_screen = Path('./tmp/screen.png')
 
 window.title("OCR Translate Project")
-# Geometry is height x width (why ??????? why not the other way around ??????
-# (nvm its always like this im dumb))
 window.geometry("900x300")
 
+def checkExistingScreen():
+   return path_screen.absolute().exists()
+
+#debug options
 def print_width():
    print("The width of Tkinter window:", window.winfo_width())
    print("The height of Tkinter window:", window.winfo_height())
 
-btn = Button(window, text="Click", command=print_width)
+def take_screenshot():
+   if checkExistingScreen():
+      screenTook = pyautogui.screenshot()
+      screenTook.save(path_screen)
+
+def popup_confirmation():
+   top = Toplevel(window)
+   top.geometry("400x200")
+   top.title("Confirm")
+   Label(top, text="Are you sure ?").place(x=160, y=80)
+
+btn = Button(window, text="Screenshot", command=take_screenshot)
 btn.place(x=80, y =200)
 
 
